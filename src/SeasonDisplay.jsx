d25030984b6062b327f2dc58f5b00dbc3f56b4d3
Alan Fidelino <afidedev26@gmail.com>
2019-06-10 16:31:19 +1000

    Ternary expression

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
 const SeasonDisplay = ({ lat }) => {
-  const style = { background: "orangered" };
+  const season = getSeason(lat, new Date().getMonth());
+
   return (
-    <div style={lat <= 0 ? style : null}>
-      <h2>Season Display Component</h2>
-      <p>Your location:{lat}</p>
+    <div>
+      {season === "winter" ? (
+        <h2>Burr, it is chilly</h2>
+      ) : (
+        <h2>Let's hit the beach</h2>
+      )}
     </div>
   );
 };

e2142100b5868d34bf841eadb532dd78f899ec08
Alan Fidelino <afidedev26@gmail.com>
2019-06-10 16:54:22 +1000

    Refactor conditional rendering of icons using config

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr!!! It's chilly",
+    iconName: "snowflake"
+  }
+};
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -9,15 +19,12 @@ const getSeason = (lat, month) => {
 };
 const SeasonDisplay = ({ lat }) => {
   const season = getSeason(lat, new Date().getMonth());
-  const text =
-    season === "winter" ? `Burr!!! It's chilly` : `Let's hit the beach!`;
-  const icon =
-    season === "winter" ? `icon snowflake massive` : `icon sun massive`;
+  const { text, iconName } = seasonConfig[season];
   return (
     <div>
-      <i className={icon} />
+      <i className={`icon massive ${iconName}`} />
       <h1>{text}</h1>
-      <i className={icon} />
+      <i className={`icon massive ${iconName}`} />
     </div>
   );
 };
